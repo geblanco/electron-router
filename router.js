f08fe6f3c4d7daa2b522a514befd7a48e63f4678
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2016-03-27 22:55:34 +0200

    Continue
    Fixes, added better param handling.
    Support for bindings on any side of the ipc.

--- router.js
+++ router.js
@@ -1,17 +1,129 @@
 'use strict'
 
-// Dependencies
-var ipc 	= null;
-var VERBS 	= ['GET', 'POST', 'UPDATE', 'DELETE'];
+var ipc = null;
+const VERBS  = ['GET', 'POST', 'UPDATE', 'DELETE'];
+var CACHED = { std: [], http: {} };
+
+const PROC_SIDES = { 'RENDERER': 0, 'MAIN': 1, TR: { 0: 'RENDERER', 1: 'MAIN' } };
+const SIDE_EVTS = {
+	0: { GET: 'ROUTER_REND_SET_ROUTES', SET: 'ROUTER_REND_GET_ROUTES' },
+	1: { GET: 'ROUTER_MAIN_SET_ROUTES', SET: 'ROUTER_MAIN_GET_ROUTES' }
+}
+var PROC_SIDE = 'MAIN';
+
+function _wrap( route ){
+	var ret = {};
+	Object.keys( route ).forEach( r => {
+		ret[r] = {
+			http: Object.keys( route[r].http ).map( h => {
+				return h;
+			})
+		};
+		if( route[r].std.length ){
+			ret[r].std = route[r].std.length
+		}
+	})
+	return JSON.stringify( ret );
+}
+
+function _storeOnCache( route ){
+	route = JSON.parse( route );
+	Object.keys( route ).forEach( r => {
+		if( route[r].std && (-1 === CACHED.std.indexOf( r )) ){
+			CACHED.std.push( r );
+		}
+		route[r].http.forEach( h => {
+			if( (-1 === CACHED.http[h].indexOf( r )) ){
+				CACHED.http[h].push( r );
+			}
+		})
+	})
+}
+
+function _hashRouteListen( verb, route, back ){
+	verb  = verb.trim().toUpperCase();
+	route = route.trim()//;.toUpperCase();
+	back = (back || false);
+	if( verb === 'STD' || (3 > arguments.length)){
+		return verb + '::' + route + '::' + PROC_SIDES.TR[PROC_SIDE];
+	}else if( !back ){
+		return verb + '::' + route + '::' + PROC_SIDES.TR[PROC_SIDE] + '::' + 'FORW';
+	}else{
+		return verb + '::' + route + '::' + PROC_SIDES.TR[1-PROC_SIDE] + '::' + 'BACK';
+	}
+}
+
+function _hashRouteSend( verb, route, back ){
+	verb  = verb.trim().toUpperCase();
+	route = route.trim()//;.toUpperCase();
+	back = (back || false);
+	if( verb === 'STD' || (3 > arguments.length)){
+		return verb + '::' + route + '::' + PROC_SIDES.TR[1-PROC_SIDE];
+	}else if( !back ){
+		return verb + '::' + route + '::' + PROC_SIDES.TR[1-PROC_SIDE] + '::' + 'FORW';
+	}else{
+		return verb + '::' + route + '::' + PROC_SIDES.TR[PROC_SIDE] + '::' + 'BACK';
+	}
+}
 
 class Router {
 
 	constructor( name, window ) {
-	
+
 		this._name = name;
 		this._routes = {};
 		this._window = window;
-		
+
+		// Prepare CACHE
+		VERBS.forEach( verb => {
+			CACHED.http[ verb ] = [];
+		})
+
+		this._setup();
+	}
+
+	_setup(){
+		// Here we set the necessary callbacks for retrieving
+		// other side events
+		if( ipc ){
+			var that = this;
+			//console.log('[SETUP]', PROC_SIDE, 'LISTENING FOR', SIDE_EVTS[PROC_SIDE].GET);
+			// Register a handler for retrieving other's side route
+			ipc.on(SIDE_EVTS[PROC_SIDE].GET, function( event, routes ){
+				_storeOnCache( routes );
+				//console.log('RESTORE CACHE', CACHED);
+			})
+			//console.log('[SETUP]', PROC_SIDE, 'RETRIEVING FOR', SIDE_EVTS[1-PROC_SIDE].GET);
+			ipc.on(SIDE_EVTS[1-PROC_SIDE].SET, function( event, routes ){
+				that._sendCache();
+			})
+			this._requestCache();
+		}
+	}
+
+	_requestCache(){
+
+		// If we are on a different process "routes" is going to be empty
+		// so no route is going to be fired, as every method registers on 
+		// ipc too, we can reconstruct the routes based on ipc channel
+		// Reconstruct all routes
+		//console.log('[SETUP]', PROC_SIDES.TR[PROC_SIDE], 'SENDING FOR', SIDE_EVTS[PROC_SIDE].SET);
+		if( this._window ){
+			this._window.send(SIDE_EVTS[PROC_SIDE].SET);
+		}else{
+			ipc.send(SIDE_EVTS[PROC_SIDE].SET);
+		}
+
+	}
+
+	_sendCache(){
+
+		if( this._window ){
+			this._window.send( SIDE_EVTS[1-PROC_SIDE].GET, _wrap(this._routes) );
+		}else{
+			ipc.send( SIDE_EVTS[1-PROC_SIDE].GET, _wrap(this._routes) );
+		}
+
 	}
 
 	_common( argss, verb ){
@@ -23,13 +135,46 @@ class Router {
 
 		var route = args.shift();
 		var cb = args.pop();
+		verb = verb.toUpperCase();
 
 		if( typeof cb === 'function' ){
 
 			this._setupRoute( route );
-			this._routes[ route ][ 'http' ][ verb.toUpperCase() ] = cb;
-		
+			this._routes[ route ][ 'http' ][ verb ] = cb;
+
 		}
+
+		//console.log('Registering route over http', verb, route, _hashRouteListen( verb, route ));
+
+		var that = this;
+
+		if( ipc ){
+
+			ipc.on( _hashRouteListen( verb, route ), function( event, req ){
+				
+				req = JSON.parse( req );
+				//console.log('Inside receiver cb', req);
+				req.push({
+					json: function( err, obj ){
+						if( arguments.length === 1 ){
+							obj = err;
+							err = null;
+						}
+						//console.log('sending to', this._window?'window':'ipc', _hashRouteSend( verb, route, true ));
+						if( this._window ){
+							this._window.send.apply(this._window, [_hashRouteSend( verb, route, true ), err].concat(JSON.parse(JSON.stringify( obj ))));
+						}else{
+							ipc.send.apply(ipc, [_hashRouteSend( verb, route, true ), err].concat(JSON.parse(JSON.stringify( obj ))));
+						}
+					}.bind(that)
+				});
+				cb.apply( that, req );
+			
+			});
+
+		}
+
+		this._sendCache();
 
 	}
 
@@ -40,45 +185,64 @@ class Router {
 		}
 
 		this._routes[ route ] = {
-			std: {
-				_senders: [],
-				_receivers: [],
-			},
+			std: [],
 			http: {}
 		};
 
 	}
 
+	// SENDER
 	send(){
 
-		var args = Array.prototype.slice.call(arguments, 0);
+		// Re-Cache, just in case...
+		this._requestCache();
 
-		if( this._window ){
-			this._window.send.apply( this._window, args || [] );
-		}
+		var that = this;
+		var fnArgs = arguments;
 
-		var routes = [args.shift()];
-		if( routes[0] === '*' ){
-			routes = routes.concat( Object.keys( this._routes ) );
-		}else{
-			routes = ['*'].concat( routes );
-		}
+		setTimeout(function() {
 
-		routes.forEach(function( route ){
+			var args = Array.prototype.slice.call(fnArgs, 0);
 
-			// Not registered
-			if( this._routes.hasOwnProperty( route ) ){
-				
-				this._routes[ route ][ 'std' ][ '_receivers' ].forEach(function( cb ){
-				
-					cb.apply( null, args || [] );
-				
-				})
-
+			if( that._window ){
+				that._window.send.apply( that._window, args || [] );
 			}
-		}, this)
+
+			var routes = [args.shift()];
+			if( routes[0] === '*' ){
+				routes = routes.concat( Object.keys( that._routes ) );
+			}else{
+				routes = ['*'].concat( routes );
+			}
+
+			routes.forEach(function( route ){
+
+				if( ipc && (-1 !== CACHED.std.indexOf(route) )){
+
+					if( that._window ){
+						that._window.send.apply(that._window, [_hashRouteSend( 'STD', route )].concat( JSON.stringify(args || []) ));
+					}else{
+						ipc.send.apply(ipc, [_hashRouteSend( 'STD', route )].concat( JSON.stringify(args || []) ));
+					}
+
+				}
+				// Not registered
+				if( that._routes.hasOwnProperty( route ) ){
+					
+					that._routes[ route ][ 'std' ].forEach(function( cb ){
+					
+						cb.apply( null, args || [] );
+					
+					})
+
+				}
+			}, that)
+
+		}, 10);
+
 	}
 
+	// RECEIVER
 	on(){
 
 		var args = Array.prototype.slice.call(arguments, 0);
@@ -92,14 +256,14 @@ class Router {
 		if( typeof endCb === 'function' ){
 
 			this._setupRoute( route );
-			this._routes[ route ][ 'std' ][ '_receivers' ].push( endCb );
+			this._routes[ route ][ 'std' ].push( endCb );
 			
 			if( ipc ){
 
-				ipc.on( route, function(){
+				ipc.on( _hashRouteListen( 'STD', route ), function(){
 
-					var args = Array.prototype.slice.call(arguments, 1);
-					endCb.apply( null, args );
+					args = Array.prototype.slice.call(arguments, 1);
+					endCb.apply( null, JSON.parse(args) );
 
 				})
 
@@ -107,8 +271,11 @@ class Router {
 
 		}
 
+		this._sendCache();
+
 	}
 
+	// SENDER
 	route(){
 		
 		var args = Array.prototype.slice.call(arguments, 0);
@@ -127,40 +294,66 @@ class Router {
 
 		var cb = args.pop();
 
-		// Not registered
-		if( !this._routes.hasOwnProperty( route ) ){
-			return;
-		}
+		// Re-Cache, just in case...
+		this._requestCache();
 
-		if( verbs[0] === '*' ){
-			verbs = VERBS;
-		}
+		var that = this;
+		setTimeout(function() {
 
-		verbs.forEach(function( verb ){
+			if( verbs[0] === '*' ){
+				verbs = VERBS;
+			}
 
-			if( this._routes[ route ][ 'http' ][verb] &&
-				typeof this._routes[ route ][ 'http' ][verb] === 'function'
-			){
-
-				this._routes[ route ][ 'http' ][ verb ].apply( null, [{
+			verbs.forEach(function( verb ){
+				
+				var fnArgs = [{
 					method: verb,
 					params: JSON.parse( JSON.stringify(params) )
-				}, {
-					json: function( err, obj ){
-						if( arguments.length === 1 ){
-							obj = err;
-							err = null;
-						}
-						cb( err, JSON.parse( JSON.stringify( obj ) ) );
+				}];
+
+				//console.log('Routing', verb, route, _hashRouteSend( verb, route ), fnArgs);
+
+				if( ipc && (-1 !== CACHED.http[verb].indexOf(route) )){
+
+					ipc.once(_hashRouteListen( verb, route, true ), function(){
+						var args = Array.prototype.slice.call( arguments, 1 );
+						cb( args.splice(0, 1)[0], JSON.parse( JSON.stringify( args ) ) );
+					})
+					//console.log('send', 'registered back on', _hashRouteListen( verb, route, true ));
+					if( that._window ){
+						that._window.send.apply(that._window, [_hashRouteSend( verb, route )].concat( JSON.stringify(fnArgs || []) ));
+					}else{
+						ipc.send.apply(ipc, [_hashRouteSend( verb, route )].concat( JSON.stringify(fnArgs || []) ));
 					}
-				}])
 
-			}//Skip
+				}
+				if(
+					that._routes.hasOwnProperty( route ) &&
+					that._routes[ route ][ 'http' ][verb] &&
+					typeof that._routes[ route ][ 'http' ][verb] === 'function'
+				){
 
-		}, this);
+					fnArgs = fnArgs.concat({
+						json: function( err, obj ){
+							console.log('inner json route', err, obj, arguments);
+							if( arguments.length === 1 ){
+								obj = err;
+								err = null;
+							}
+							cb( err, JSON.parse( JSON.stringify( obj ) ) );
+						}
+					})
+					that._routes[ route ][ 'http' ][ verb ].apply( null, fnArgs );
+
+				}// Not registered, skip on std router
+
+			}, that);
+
+		}, 10);
 
 	}
 
+	// RECEIVERS
 	post(){
 
 		this._common( arguments, 'POST' );
@@ -190,7 +383,17 @@ class Router {
 var _router = null;
 module.exports = function( name, window ){
 	try{
+
 		ipc = require('electron').ipcMain;
+
+		if( ipc !== undefined ){
+			// We are on main process
+			PROC_SIDE = PROC_SIDES.MAIN;
+		}else{
+			ipc = require('electron').ipcRenderer;
+			PROC_SIDE = PROC_SIDES.RENDERER;
+		}
+
 	}catch( e ){
 		// Non electron environment
 	}
