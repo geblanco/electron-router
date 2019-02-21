fcaaf58888a1350f1f9aa2d16a2f2d477a39fcdb
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2019-02-21 21:49:34 +0100

    Update dependencies, fix all vulnerabilities

--- tests/renderer/process.js
+++ tests/renderer/process.js
@@ -4,7 +4,7 @@
 const should = require('should')
 /* eslint-enable no-unused-vars */
 const path = require('path')
-const router = require(path.join(__dirname, '/../../router'))('TEST')
+const router = require(path.join(__dirname, '/../../../router'))('TEST')
 const async = require('async')
 const reqResult = 'ok'
 const baseData = '5js2q4k'
@@ -24,13 +24,19 @@ const duplexMethods = [
 // global.DEBUG = 'route'
 
 let prepareSimpleCommTests = (done) => {
-  for (let i = 0; i < data.length; i++) {
-    router.clean()
-    router.on(`simplex-communication-data-types-${i}`, () => {
-      router.send(`simpleCommTest-${i}`, data[ i ])
-      if (i === data.length - 1) done()
-    })
-  }
+  let i = 0
+  async.whilst(
+    () => i < data.length,
+    (callback) => {
+      router.clean()
+      router.on(`simplex-communication-data-types-${i}`, () => {
+        router.send(`simpleCommTest-${i}`, data[ i ])
+        i++
+        callback()
+      })
+    },
+    () => done()
+  )
 }
 
 let prepareMultipleCallsTest = (done) => {
@@ -43,29 +49,43 @@ let prepareMultipleCallsTest = (done) => {
 }
 
 let prepareDuplexCommTests = (done) => {
-  for (let i = 0; i < data.length; i++) {
-    router.clean()
-    router.on(`doubleCommTest-${i}`, () => {
-      router.route(duplexMethods[ i ], 'doubleCommTest', data[ i ], (err, result) => {
-        should.not.exist(err)
-        result.should.be.a.String().and.be.exactly(reqResult)
-        router.send(`doubleCommTest-${i}-end`)
-        if (i === data.length - 1) done()
+  let i = 0
+  async.whilst(
+    () => i < data.length,
+    (callback) => {
+      router.clean()
+      router.on(`doubleCommTest-${i}`, () => {
+        router.route(duplexMethods[ i ], 'doubleCommTest', data[ i ], (err, result) => {
+          should.not.exist(err)
+          result.should.be.a.String().and.be.exactly(reqResult)
+          router.send(`doubleCommTest-${i}-end`)
+          i++
+          callback()
+        })
       })
-    })
-  }
+    },
+    () => done()
+  )
 }
 
 let prepareDuplexCommMultipleCallsTests = (done) => {
   router.on('doubleCommTest-multiple-calls', (testCalls) => {
-    router.clean()
-    for (let i = 0; i < testCalls; i++) {
-      router.route('get', 'doubleComm-multiple-calls', { id: '5js2q4k' }, (err, result) => {
-        should.not.exist(err)
-        result.should.be.a.String().and.be.exactly(reqResult)
-        if (i === testCalls - 1) { router.send('doubleCommTest-multiple-calls-end'); done() }
-      })
-    }
+    let i = 0
+    async.whilst(
+      () => i < testCalls,
+      (callback) => {
+        router.route('get', 'doubleComm-multiple-calls', { id: '5js2q4k' }, (err, result) => {
+          should.not.exist(err)
+          result.should.be.a.String().and.be.exactly(reqResult)
+          i++
+          callback()
+        })
+      },
+      () => {
+        router.send('doubleCommTest-multiple-calls-end')
+        done()
+      }
+    )
   })
 }
 
